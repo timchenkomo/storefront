c8fc66aeff2bc5facce2c916acaa1a72e6d206f2
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-04 15:36:25 +0300

    NavBar component

---
+++ components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-auto py-6 border-b-2 mb-12">
+    <div class="container mx-auto flex items-center justify-between px-4 text-sm font-light">
+      <div>
+        <img src="http://bbt-online.ru/wp-content/uploads/logo.svg">
+      </div>
+
+      <nav>
+        <span class="px-4">
+          Об издательстве
+        </span>
+        <span class="px-4">
+          Библиотека
+        </span>
+        <span class="px-4">
+          Войти
+        </span>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+class NavBar extends Vue {
+  @Prop() readonly price!: string;
+}
+
+export default NavBar
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,19 @@
 <template>
-  <nuxt />
+  <div>
+    <nav-bar />
+    <nuxt />
+  </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import NavBar from '@/components/NavBar.vue'
+
+@Component({
+  components: { NavBar }
+})
+class DefaultLayout extends Vue {
+}
+
+export default DefaultLayout
+</script>
