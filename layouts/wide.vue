52dc786315fcd31cdb0a059c44dc626e7c5e84ad
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-05 12:46:07 +0300

    About page

---
+++ layouts/wide.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav-bar />
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import NavBar from '@/components/NavBar.vue'
+
+@Component({
+  components: { NavBar }
+})
+class WideLayout extends Vue {
+}
+
+export default WideLayout
+</script>
