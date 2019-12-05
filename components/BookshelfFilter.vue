5af74229b2b527afd16fa661d267f699ba22eea7
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-05 13:35:22 +0300

    Bokshelf filter

--- components/BookshelfFilter.vue
+++ components/BookshelfFilter.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <input v-model.trim="value" @input="onValueChanged" class="p-2 border border-solid rounded w-full">
+  <div @click="onFocused" class="p-2 border border-solid rounded flex items-center justify-center">
+    <!-- Search icon -->
+    <img
+      class="h-4 mx-2"
+      src="http://bbt-online.ru/wp-content/themes/bbt/img/vector/search.svg"
+    >
+
+    <!-- Placeholder -->
+    <span
+      v-show="!focused"
+      class="text-gray-400"
+    >
+      Введите название книги или автора
+    </span>
+
+    <!-- Input field -->
+    <input
+      ref="input"
+      v-show="focused"
+      v-model.trim="value"
+      @input="onValueChanged"
+      @blur="onBlur"
+      class="w-full mx-2"
+    >
   </div>
 </template>
 
@@ -10,9 +32,21 @@ import { Vue, Component } from 'vue-property-decorator'
 @Component
 class BookshelfFilter extends Vue {
   private value:string = '';
+  private focused:boolean = false;
 
   private onValueChanged() {
     this.$emit('input', this.value)
+  }
+
+  private onFocused() {
+    this.focused = true
+    this.$nextTick(function () {
+      this.$refs.input.focus()
+    })
+  }
+
+  private onBlur() {
+    this.focused = (this.value !== '')
   }
 }
 
