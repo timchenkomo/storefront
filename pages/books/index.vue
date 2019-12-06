fa7fa1ca43d75b4c4fb18bdfae17a5eca6051975
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-06 17:54:30 +0300

    fetch data at library page

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -8,7 +8,8 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
+import axios from 'axios'
 import { Book } from '@/lib/book'
 
 import Bookshelf from '@/components/Bookshelf.vue'
@@ -18,43 +19,20 @@ import BookshelfFilter from '@/components/BookshelfFilter.vue'
   components: { Bookshelf, BookshelfFilter }
 })
 class BooksIndexPage extends Vue {
-  private query:string = '';
+  private query: string = '';
+  private allBooks: Book[] = [];
+
+  async asyncData() {
+    const { data } = await axios.get('http://localhost:8000/books')
+    return { allBooks: data }
+  }
 
   get books(): Book[] {
-    return [
-      {
-        id: 'bg',
-        title: 'Бхагавад-гита',
-        author: 'А.Ч. Бхактиведанта Свами Прабхупада',
-        cover: 'http://bbt-online.ru/wp-content/uploads/Bhagavad-Gita-2.jpg'
-      },
-      {
-        id: 'ns',
-        title: 'Наука самоосознания',
-        author: 'А.Ч. Бхактиведанта Свами Прабхупада',
-        cover: 'http://bbt-online.ru/wp-content/uploads/Nauka-Samoosoznaniya-1.jpg'
-      },
-      {
-        id: 'vv',
-        title: 'Высший вкус',
-        author: '',
-        cover: 'http://bbt-online.ru/wp-content/uploads/Vysshiy-Vkus-1.jpg'
-      },
-      {
-        id: 'mh1',
-        title: 'Махабхарата, том первый',
-        author: 'Кришна-дхарма дас',
-        cover: 'http://bbt-online.ru/wp-content/uploads/Mahabharata-1-1.jpg'
-      },
-      {
-        id: 'mh2',
-        title: 'Махабхарата, том второй',
-        author: 'Кришна-дхарма дас',
-        cover: 'http://bbt-online.ru/wp-content/uploads/Mahabharata-2-1.jpg'
-      }
-    ].filter(x =>
-      x.title.toLowerCase().includes(this.query.toLowerCase()) ||
-      x.author.toLowerCase().includes(this.query.toLowerCase()))
+    return this.allBooks.filter(
+      x =>
+        x.title.toLowerCase().includes(this.query.toLowerCase()) ||
+        x.author.toLowerCase().includes(this.query.toLowerCase())
+    )
   }
 }
 
