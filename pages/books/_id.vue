285c8a5881ba5e529191c73b054746b660ce7d42
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-06 17:54:43 +0300

    fetch data for book

--- pages/books/_id.vue
+++ pages/books/_id.vue
@@ -2,19 +2,19 @@
   <div class="flex flex-row xl:mx-8">
     <!-- Cover -->
     <div class="w-1/3 mr-16">
-      <img class="rounded" src="http://bbt-online.ru/wp-content/uploads/Mahabharata-1-1.jpg">
+      <img :src="book.cover" class="rounded">
     </div>
 
     <!-- Content -->
     <div class="w-2/3">
       <h1 class="font-serif text-5xl leading-none mb-3">
-        Махабхарата, том первый
+        {{ book.title }}
       </h1>
       <div class="text-sm text-gray-500">
-        Автор: <a href="#">в пересказе Кришна-Дхармы</a>
+        Автор: <a href="#">{{ book.author }}</a>
       </div>
       <div class="font-light text-sm text-gray-900 mt-4 leading-loose">
-        «Махабхарата» Кришна-Дхармы даса представляет собой художественное изложение самой сути одноименного многотомного эпоса. Пересказав сокращенно древнюю поэму, но не упустив при этом ни одной важной детали, автор дает читателю замечательную возможность погрузиться в атмосферу того великого времени, когда на Земле присутствовал Сам Кришна.
+        {{ book.desc }}
       </div>
 
       <!-- Buttons -->
@@ -39,7 +39,8 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
+import axios from 'axios'
 import IconButton from '@/components/IconButton.vue'
 import BuyButton from '@/components/BuyButton.vue'
 import InCartButton from '@/components/InCartButton.vue'
@@ -50,6 +51,11 @@ import VarierySwitcher from '@/components/VarietySwitcher.vue'
 })
 class BookPage extends Vue {
   private activeVariery: string = 'digital';
+
+  async asyncData(ctx) {
+    const { data } = await axios.get('http://localhost:8000/books/' + ctx.params.id)
+    return { book: data }
+  }
 
   private get varieties() {
     return [
