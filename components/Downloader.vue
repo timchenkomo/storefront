5805f3f1ef51fed65f38540dd8df4f312d09bcc1
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-22 14:16:05 +0300

    Downloader component

---
+++ components/Downloader.vue
@@ -0,0 +1,32 @@
+<template functional>
+  <div
+    v-if="props.visible"
+    @click="listeners.close"
+    class="fixed flex inset-0 w-full h-screen items-center justify-center"
+  >
+    <!-- Modal dialog -->
+    <div class="w-full max-w-xl bg-white shadow-lg rounded-lg p-4">
+      <!-- Title -->
+      <h3 class="text-xl">
+        {{ props.title }}
+        <span @click="listeners.close" class="float-right cursor-pointer">
+          <close-icon class="w-4 h-4" />
+        </span>
+      </h3>
+      <hr>
+
+      <!-- List of formats -->
+      <ul class="mt-4">
+        <a
+          v-for="(opt, idx) in props.options"
+          :href="opt.url"
+          :key="idx"
+        >
+          <li class="hover:bg-blue-500 rounded px-4 py-1 my-1 hover:text-white">
+            Скачать {{ opt.type }}
+          </li>
+        </a>
+      </ul>
+    </div>
+  </div>
+</template>
