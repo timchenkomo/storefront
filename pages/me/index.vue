739b36694b341b3b964cce46c4dc5963d5e79ce4
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-22 16:21:55 +0300

    Handle Downloader popup when download button clicked only, not whole cover

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -8,11 +8,11 @@
     <bookshelf :books="myFilteredProducts">
       <!-- Add downlaod button to each book. -->
       <template v-slot:cover="product">
-        <div
-          @click="onProductClicked(product)"
-          class="absolute flex justify-center items-center w-full h-full opacity-0 hover:opacity-100"
-        >
-          <div class="p-4 bg-blue-500 hover:bg-blue-400 rounded-full">
+        <div class="absolute flex justify-center items-center w-full h-full opacity-0 hover:opacity-100">
+          <div
+            @click="onProductClicked(product)"
+            class="p-4 bg-blue-500 hover:bg-blue-400 rounded-full cursor-pointer"
+          >
             <download-icon class="w-6 h-6 fill-current text-white" />
           </div>
         </div>
