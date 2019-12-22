62325a39e1189f76f049963365f3fdef945afcc3
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-12-22 14:14:55 +0300

    Icon components plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    '~/plugins/icons'
+  ],
   /*
    ** Nuxt.js dev-modules
    */
---
+++ plugins/icons.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import CloseIcon from '~/assets/close.svg'
+
+Vue.component('close-icon', CloseIcon)
