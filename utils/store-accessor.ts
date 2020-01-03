f2a14ca2747a92f9fd0d0c6935e60d85e2fe3559
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-03 13:21:56 +0300

    Msg store registered

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -1,14 +1,18 @@
 import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import CartModule from '~/store/cart'
+import MsgModule from '~/store/msg'
 
 let cartStore: CartModule
+let msgStore: MsgModule
 
 function initialiseStores(store: Store<any>): void {
   cartStore = getModule(CartModule, store)
+  msgStore = getModule(MsgModule, store)
 }
 
 export {
   initialiseStores,
-  cartStore
+  cartStore,
+  msgStore
 }
