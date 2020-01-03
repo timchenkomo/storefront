8af6a7316c0a6e806961dce38497f5e18e7dd43d
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-03 13:21:26 +0300

    Msg component registered

--- plugins/ui-kit.ts
+++ plugins/ui-kit.ts
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import BookCard from '~/components/BookCard.vue'
 import SignInForm from '~/components/SignInForm.vue'
 import SignUpForm from '~/components/SignUpForm.vue'
+import Msg from '~/components/Msg.vue'
 
 Vue.component('book-card', BookCard)
 Vue.component('login-form', SignInForm)
 Vue.component('signup-form', SignUpForm)
+Vue.component('msg', Msg)
