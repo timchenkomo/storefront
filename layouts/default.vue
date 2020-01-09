0a42bf65c8eba2080e60e2a4a19b49bce5a0ec51
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-09 13:07:15 +0300

    Show list of messages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="font-montserrat">
     <nav-bar />
-    <div class="mx-4">
+    <div class="mx-4 lg:mx-32">
       <div class="mx-auto mt-6">
+        <!-- Flash messages -->
+        <msg
+          v-for="(msg, idx) in messages"
+          @click="onMsgClicked(idx)"
+          :key="idx"
+          :msg="msg.msg"
+          class="my-2"
+        />
         <nuxt />
       </div>
     </div>
@@ -12,11 +20,20 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import NavBar from '@/components/NavBar.vue'
+import { msgStore } from '@/store/index'
+import { Message } from '@/lib/msg'
 
 @Component({
   components: { NavBar }
 })
 class DefaultLayout extends Vue {
+  get messages(): Message[] {
+    return msgStore.messages
+  }
+
+  private onMsgClicked(idx: number) {
+    msgStore.removeByIdx(idx)
+  }
 }
 
 export default DefaultLayout
