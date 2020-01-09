0f3d4686de92415897c6d2119c1c3d0196b943c7
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-09 13:05:47 +0300

    Show message and riderect users after signup.

--- pages/me/signup.vue
+++ pages/me/signup.vue
@@ -11,12 +11,20 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import axios from 'axios'
+
+import { msgStore } from '@/store/index'
 import { SignUpForm } from '@/lib/forms.ts'
 
 @Component
 class SignUpPage extends Vue {
   private async onSignUpClicked(form: SignUpForm) {
-    await axios.post('http://localhost:8000/me/signup', form)
+    const res = await axios.post('http://localhost:8000/me/signup', form)
+
+    // registration was successfull
+    if (res.data.success) {
+      msgStore.add({ msg: 'Вы были успешно зарегистрированы!' })
+      this.$router.push('/books')
+    }
   }
 }
 
