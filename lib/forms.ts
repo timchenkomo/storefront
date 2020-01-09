427b10e70c2597ddbc4842e04e861e3838545f66
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-09 13:29:10 +0300

    API forms

---
+++ lib/forms.ts
@@ -0,0 +1,11 @@
+export interface SignUpForm {
+  name: string,
+  login: string,
+  password: string,
+  promocode: string
+}
+
+export interface SignInForm {
+  login: string
+  password: string
+}
