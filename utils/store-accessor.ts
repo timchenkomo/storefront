2d0e13984112cbe1d7a053a05524e53368ceb7ea
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-24 11:12:19 +0300

    UserStore was replaced by Nuxt Auth

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -2,21 +2,17 @@ import { Store } from 'vuex'
 import { getModule } from 'vuex-module-decorators'
 import CartModule from '~/store/cart'
 import MsgModule from '~/store/msg'
-import UserModule from '~/store/user'
 
 let cartStore: CartModule
 let msgStore: MsgModule
-let userStore: UserModule
 
 function initialiseStores(store: Store<any>): void {
   cartStore = getModule(CartModule, store)
   msgStore = getModule(MsgModule, store)
-  userStore = getModule(UserModule, store)
 }
 
 export {
   initialiseStores,
   cartStore,
   msgStore,
-  userStore
 }
