17071f653100a4c4d6c3248f21fb6573dd96e981
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-01-29 17:57:29 +0300

    Restore password routine

--- routes/me.py
+++ routes/me.py
@@ -1,18 +1,20 @@
 from datetime import timedelta
 from typing import Dict
 
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-
-from auth import (ACCESS_TOKEN_EXPIRE_MINUTES, authenticate_user,
-                  create_access_token, get_current_active_user, PWD_CONTEXT)
+from auth import (ACCESS_TOKEN_EXPIRE_MINUTES, PWD_CONTEXT, authenticate_user,
+                  create_access_token, create_ot_access_token,
+                  find_ot_access_token, get_current_active_user)
 from db import db_session
-from db.models import User
+from db.models import AccessToken, User
+from fastapi import APIRouter, Depends, HTTPException
 from forms.auth import Token
 from forms.products import Group
-from forms.user import SignIn, SignUp, UserInfo
+from forms.user import (ChangePasswordRequest, RestorePasswordRequest, SignIn,
+                        SignUp, UserInfo)
 from mappers.products import model2group_nv, model2product
 from mappers.user import model2user
+from postman import send_email
+from sqlalchemy.orm import Session
 
 router = APIRouter()  # pylint: disable=invalid-name
 
@@ -78,3 +80,37 @@ async def user_get_products(user: User = Depends(get_current_active_user)):
 
         result[product_group.id].products.append(model2product(product))
     return list(result.values())
+
+
+@router.post(
+    "/password/restore"
+)
+async def user_restore_password(
+        form: RestorePasswordRequest,
+        db: Session = Depends(db_session)):
+    """Sends a restore email to user."""
+    user: User = db.query(User).filter(User.email == form.email).first()
+    if user:
+        token = create_ot_access_token(user)
+        db.add(token)
+        db.commit()
+        send_email(user.email, "Here is your new password.")
+        return {"success": True}
+    else:
+        return {"success": False, "msg": "No user found"}
+
+
+@router.post(
+    "/password/change"
+)
+async def user_change_password(
+        form: ChangePasswordRequest,
+        db: Session = Depends(db_session)):
+    token: AccessToken = find_ot_access_token(db, form.token)
+    if not token:
+        return {"success": False}
+
+    token.user.hashed_password = PWD_CONTEXT.hash(form.password)
+    db.delete(token)
+    db.commit()
+    return {"success": True}
