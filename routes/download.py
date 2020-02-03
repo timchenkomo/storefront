6bca3df1f52c43a19c77725b06878377ffa4e16a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-02-03 10:46:25 +0300

    /download route refactoring

--- routes/download.py
+++ routes/download.py
@@ -1,35 +1,39 @@
 from os import environ
 from os.path import join
 
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 from starlette.responses import FileResponse
 
 from auth import get_current_user_by_cookie
 from db import db_session
 from db.models import Product, Purchase, User
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
 
 ROOT = environ.get("DOWNLOADS_PATH", "../downloads")
 
 router = APIRouter()  # pylint: disable=invalid-name
 
+# Sample : /sample/{product_slug}.{ext} : {product_slug}/sample.{ext}
+# Product: /{product_slug}.{ext}        : {product_slug}/{product_slug}.{ext}
+# Reader : /{product_slug}.epub         : {product_slug}/{product_slug}.epub
+
 
 @router.get(
-    "/{product_slug}/sample_{filename}",
+    "/{product_slug}/sample.{ext}",
     summary="Download a sample of a product."
 )
-async def download_sample(product_slug: str, filename: str) -> FileResponse:
+async def download_sample(product_slug: str, ext: str) -> FileResponse:
     """Download a sample."""
-    return FileResponse(join(ROOT, product_slug, "sample_" + filename))
+    return FileResponse(join(ROOT, product_slug, "sample." + ext))
 
 
 @router.get(
-    "/{product_slug}/{filename}",
+    "/{product_slug}\.{ext}",  # noqa pylint: disable=W1401
     summary="Download a product."
 )
 async def download_product(
         product_slug: str,
-        filename: str,
+        ext: str,
         user: User = Depends(get_current_user_by_cookie),
         db: Session = Depends(db_session)) -> FileResponse:
     """Download a product file if user have bought it."""
@@ -45,4 +49,4 @@ async def download_product(
     if not purchase:
         raise HTTPException(status_code=403, detail="Doesn't belong to you")
 
-    return FileResponse(join(ROOT, product.group.slug, filename))
+    return FileResponse(join(ROOT, product_slug, product_slug + "." + ext))
