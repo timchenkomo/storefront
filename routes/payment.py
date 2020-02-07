d869b5b0b138106b8cdf0886d896dab4deb63787
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-02-07 16:23:53 +0300

    Process callback from robokassa

--- routes/payment.py
+++ routes/payment.py
@@ -1,8 +1,10 @@
 from os import environ
 from hashlib import md5
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException, Form, Depends
 
 from db import db_session
-from fastapi import APIRouter, Depends, HTTPException, Query
+from db.models import Product, Purchase, User
 
 ROOT = environ.get("DOWNLOADS_PATH", "../downloads")
 PASS1 = environ.get("MRH_PASS_1", "BJYhRoXsT454wP7aEz5y")
@@ -12,20 +14,28 @@ router = APIRouter()  # pylint: disable=invalid-name
 
 
 @router.post(
-    "/payment",
+    "/result_pay",
     summary="Payment callback"
 )
 async def payment_result(
-        out_sum: float = Query(0, alias="OutSum"),
-        inv_id: str = Query(None, alias="InvId"),
-        crc: str = Query(None, alias="SignatureValue")):
+        out_summ: float = Form(0),
+        inv_id: str = Form(None),
+        crc: str = Form(None),
+        db: Session = Depends(db_session)):
     """Payment callback."""
+    # truncate zeros from the end
+    out_summ2 = str(round(out_summ)).rstrip('0').rstrip('.')
     my_crc = md5(
-        f"{out_sum}:{inv_id}:{PASS1}".encode("utf-8")
+        f"{out_summ2}:{inv_id}:{PASS2}".encode("utf-8")
     ).hexdigest().upper()
-    print(my_crc, crc, f"{out_sum}:{inv_id}:{PASS1}")
 
     if (my_crc != crc):
         raise HTTPException(status_code=401, detail="Bad sign")
 
-    return {"msg": "Thank you for using our service"}
+    # update flag
+    db.query(Purchase) \
+        .filter(Purchase.invoice_id == inv_id) \
+        .update({Purchase.paid: True})
+    db.commit()
+
+    return "OK" + inv_id
