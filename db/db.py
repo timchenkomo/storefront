e0fba00638de4e7fc02dd296f94dc512a7632e61
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-02-09 18:17:18 +0300

    get db connection string from env

--- db/db.py
+++ db/db.py
@@ -1,7 +1,11 @@
+from os import environ
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://bbtadm:bbtpass@127.0.0.1/bbt"
+
+SQLALCHEMY_DATABASE_URL = environ.get(
+    "DB_CONNECTION", "postgresql://bbtadm:bbtpass@127.0.0.1/bbt")
 
 ENGINE = create_engine(SQLALCHEMY_DATABASE_URL)
 DB_SESSION_LOCAL = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
