88b92e84a0fb90b9b36b1f927dd3e60f7e674fee
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-02-09 14:21:56 +0300

    fix connection string for CI

--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://bbtadm:bbtpass@postgres/bbt"
+SQLALCHEMY_DATABASE_URL = "postgresql://bbtadm:bbtpass@127.0.0.1/bbt"
 
 ENGINE = create_engine(SQLALCHEMY_DATABASE_URL)
 DB_SESSION_LOCAL = sessionmaker(autocommit=False, autoflush=False, bind=ENGINE)
