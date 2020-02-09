ff38f48fa07e94cd59cfce9621d359782fa74494
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-02-09 16:11:17 +0300

    landing pages after purchase

---
+++ pages/me/purchase/fail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="w-64 mb-4 text-xl text-center">
+      Ошибка при совершении покупки!
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+class PurchaseFailPage extends Vue {
+}
+
+export default PurchaseFailPage
+</script>
---
+++ pages/me/purchase/success.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="w-64 mb-4 text-xl text-center">
+      Покупка завершена!
+    </h1>
+
+    <nuxt-link to="/me">
+      Перейти к моим покупкам
+    </nuxt-link>
+    <nuxt-link to="/books">
+      Перейти в библиотеку
+    </nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+class PurchaseSuccessPage extends Vue {
+}
+
+export default PurchaseSuccessPage
+</script>
