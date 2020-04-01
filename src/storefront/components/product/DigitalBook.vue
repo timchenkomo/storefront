7876d6edfda3568e4b31b09d98ed3cc97c38fc32
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-04-01 14:27:54 +0530

    show buttons (read/download sample) according to sample_formats filed

--- src/storefront/components/product/DigitalBook.vue
+++ src/storefront/components/product/DigitalBook.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="flex flex-col sm:flex-row">
+  <div
+    v-if="canRead || hasSamples"
+    class="flex flex-col sm:flex-row"
+  >
     <!-- Read sample -->
     <nuxt-link
+      v-if="canRead"
       :to="readerUrl"
       class="w-full bg-blue-500 rounded text-white text-center px-4 py-2 my-1 mr-2 text-sm"
     >
@@ -10,11 +14,12 @@
 
     <!-- Download sample -->
     <div
-      class="bg-blue-500 rounded text-white text-center px-4 py-2 my-1 md:mr-2 text-sm whitespace-no-wrap"
+      v-if="hasSamples"
+      class="w-full bg-blue-500 rounded text-white text-center px-4 py-2 my-1 md:mr-2 text-sm whitespace-no-wrap"
     >
       {{ actionText }}
       <a
-        v-for="format in product.formats"
+        v-for="format in bought ? product.formats : product.sample_formats"
         :key="format"
         :href="bought ? productUrl(format) : sampleUrl(format)"
         class="hover:text-blue-200"
@@ -49,6 +54,18 @@ import { getSampleUrl, getProductUrl } from '@/lib/download'
 
   private get actionText(): string {
     return this.bought ? 'Скачать' : 'Фрагмент'
+  }
+
+  private get hasSamples(): boolean {
+    return !!this.product.sample_formats
+  }
+
+  private get canRead(): boolean {
+    if (!this.hasSamples) {
+      return false
+    }
+    // our online reader can read epubs only
+    return this.product.sample_formats.includes('epub')
   }
 }
 
