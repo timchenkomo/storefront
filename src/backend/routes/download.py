0c1a116dfb9feeee27c6fe1e7c1537a23dce6efe
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2020-04-01 16:31:05 +0530

    fix download path for native development

--- src/backend/routes/download.py
+++ src/backend/routes/download.py
@@ -10,7 +10,7 @@ from db import db_session
 from db.models import Product, User
 from logic.users import get_user_products
 
-ROOT = environ.get("DOWNLOADS_PATH", "../downloads")
+ROOT = environ.get("DOWNLOADS_PATH", "../../downloads")
 
 router = APIRouter()  # pylint: disable=invalid-name
 
